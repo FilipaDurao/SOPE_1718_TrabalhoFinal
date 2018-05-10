57a7cdc0fada70f6c15d13702f972b74434f1ea3
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-10 21:53:16 +0100

    Added function to map error codes to descriptions (XXX)

--- log.c
+++ log.c
@@ -5,6 +5,35 @@ FILE* sLogFile;
 FILE* cBookFile;
 FILE* cLogFile;
 
+/**
+ * @brief Maps an error code to an error abreviation string
+ * 
+ * @param error The error code
+ */
+static char* getErrorDescription(int error) {
+    switch(error) {
+        case -1: 
+            return "MAX";
+
+        case -2:
+            return "NST";
+
+        case -3:
+            return "IID";
+
+        case -4:
+            return "ERR";
+
+        case -5:
+            return "NAV";
+
+        case -6:
+            return "FUL";
+
+        case -7:
+            return "OUT";
+    }
+}
 int openFiles(){
 
     if((sBookFile = fopen(SERVER_BOOK_FILENAME, "a")) == NULL){
@@ -30,6 +59,6 @@ int clientLogBookSuccess(int clientID) {
 
 }
 
-int clientLogBookFailed(int clientID, struct) {
+int clientLogBookFailed(int clientID) {
 
 }
