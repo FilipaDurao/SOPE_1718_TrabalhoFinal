abd9d35f9a24e8ce9791ce8e7e83e3e095aad891
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-14 00:53:25 +0100

    Added Filipa's log code

--- log.c
+++ log.c
@@ -7,12 +7,12 @@ FILE* cBookFile;
 
 /**
  * @brief Maps an error code to an error abreviation string
- * 
+ *
  * @param error The error code
  */
 static char* getErrorDescription(int error) {
     switch(error) {
-        case -1: 
+        case -1:
             return "MAX";
 
         case -2:
@@ -34,7 +34,7 @@ static char* getErrorDescription(int error) {
             return "OUT";
 
         default:
-            return "";    
+            return "";
     }
 }
 int openFiles(){
@@ -57,8 +57,8 @@ void clientLogBookSuccess(int clientID, int num_booked_seats, int *booked_seats)
     FILE* cLogFile = fopen(CLIENT_LOG_FILENAME, "a");
 
     for(int i = 1; i <= num_booked_seats; i++) {
-        fprintf(cLogFile, "%0*d %0*d.%0*d %0*d\n", 
-            WIDTH_PID, clientID, 
+        fprintf(cLogFile, "%0*d %0*d.%0*d %0*d\n",
+            WIDTH_PID, clientID,
             (WIDTH_XXNN-1)/2, i,
             (WIDTH_XXNN-1)/2, num_booked_seats,
             WIDTH_SEAT, booked_seats[i-1]);
@@ -71,4 +71,73 @@ void clientLogBookFailed(int clientID, int error) {
     FILE* cLogFile = fopen(CLIENT_LOG_FILENAME, "a");
     fprintf(cLogFile, "%0*d %s\n", WIDTH_PID, clientID, getErrorDescription(error));
     fclose(cLogFile);
+}
+
+void openTicketOfficeLog(int toNumber){
+    fprintf(sLogFile, "%04d-OPEN\n", toNumber);
+}
+
+void closeTicketOfficeLog(int toNumber){
+    fprintf(sLogFile, "%04d-CLOSED\n", toNumber);
+}
+
+void serverLogSuccess(int toNumber, int clientID, int numSeats, int* preferedSeats, int* booked_seats){
+
+    int numSpaces = MAX_CLI_SEATS*5;
+    int preferedSeatsSize = sizeof(preferedSeats)/sizeof(preferedSeats[0]);
+
+    fprintf(sLogFile, "%02d-%i-%02d:", toNumber, clientID, numSeats);
+
+    // Print the prefered Seats
+    for(int i = 0; i < preferedSeatsSize; i++){
+        fprintf(sLogFile, " %04d", preferedSeats[i]);
+        numSpaces -= 5;
+    }
+
+    // Fill the spaces to be formated
+    for(int i = numSpaces; i > 0; i--){
+        fprintf(sLogFile, " ");
+    }
+
+    fprintf(sLogFile, " -");
+
+    for(int i = 0; i < numSeats; i++){
+        fprintf(sLogFile, " %04d", booked_seats[i]);
+    }
+
+    fprintf(sLogFile, "\n");
+}
+
+void serverLogFailure(int toNumber, int clientID, int numSeats, int* preferedSeats, int error){
+
+    int numSpaces = MAX_CLI_SEATS*5;
+    int preferedSeatsSize = sizeof(preferedSeats)/sizeof(preferedSeats[0]);
+
+    fprintf(sLogFile, "%02d-%i-%02d:", toNumber, clientID, numSeats);
+
+    // Print the prefered Seats
+    for(int i = 0; i < preferedSeatsSize; i++){
+        fprintf(sLogFile, " %04d", preferedSeats[i]);
+        numSpaces -= 5;
+    }
+
+    // Fill the spaces to be formated
+    for(int i = numSpaces; i > 0; i--){
+        fprintf(sLogFile, " ");
+    }
+
+    fprintf(sLogFile, " - %s\n", getErrorDescription(error));
+
+}
+
+void closeServerLog(){
+    fprintf(sLogFile, "SERVER CLOSED");
+}
+
+void fillServerBookings(Room room){
+    for(int i = 0; i < room.numSeats; i++){
+        if(room.seats[i].status == BOOKED){
+            fprintf(sBookFile, "%04d\n", i+1);  //i+1 é o lugar, pois a sala começa no lugar 1
+        }
+    }
 }
--- log.h
+++ log.h
@@ -20,17 +20,65 @@ int openFiles();
 
 /**
  * @brief Logs a successful event on clog.txt
- * 
- * @param clientID 
- * @param num_booked_seats 
- * @param booked_seats 
+ *
+ * @param clientID
+ * @param num_booked_seats
+ * @param booked_seats
  */
 void clientLogBookSuccess(int clientID, int num_booked_seats, int *booked_seats);
 
 /**
  * @brief Logs an error event on clog.txt
- * 
- * @param clientID 
- * @param error 
+ *
+ * @param clientID
+ * @param error
  */
 void clientLogBookFailed(int clientID, int error);
+
+/**
+ * @brief Logs the opening of a ticket office on slog.txt
+ *
+ * @param toNumber
+ */
+void openTicketOfficeLog(int toNumber);
+
+/**
+ * @brief Logs the closing of a ticket office on slog.txt
+ *
+ * @param toNumber
+ */
+void closeTicketOfficeLog(int toNumber);
+
+/**
+ * @brief Logs a successful request on slog.txt
+ *
+ * @param toNumber
+ * @param clientID
+ * @param numSeats
+ * @param preferedSeats
+ * @param booked_seats
+ */
+void serverLogSuccess(int toNumber, int clientID, int numSeats, int* preferedSeats, int* booked_seats);
+
+/**
+ * @brief Logs a failed request on slog.txt
+ *
+ * @param toNumber
+ * @param clientID
+ * @param numSeats
+ * @param preferedSeats
+ * @param error
+ */
+void serverLogFailure(int toNumber, int clientID, int numSeats, int* preferedSeats, int error);
+
+/**
+ * @brief Logs the closure on slog.txt
+ */
+void closeServerLog();
+
+/**
+ * @brief Writes all the booked seats in sbook.txt
+ *
+ * @param room
+ */
+void fillServerBookings(Room room);
